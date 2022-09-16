eab5c4b747bc3abdd8534275b3109f4547af68be
Puneet <puneethkumargajjela@gmail.com>
2022-09-16 14:20:18 +0000

    addFeatures

---
+++ src/components/Cart/index.js
@@ -0,0 +1,48 @@
+import Header from '../Header'
+
+import CartContext from '../../context/CartContext'
+
+import EmptyCartView from '../EmptyCartView'
+
+import CartListView from '../CartListView'
+
+import CartSummary from '../CartSummary'
+
+import './index.css'
+
+const Cart = () => (
+  <CartContext.Consumer>
+    {value => {
+      const {cartList, removeAllCartItems} = value
+      const showEmptyView = cartList.length === 0
+      const onClickRemoveAllBtn = () => {
+        removeAllCartItems()
+      }
+
+      return (
+        <>
+          <Header />
+          {showEmptyView ? (
+            <EmptyCartView />
+          ) : (
+            <div className="cart-container">
+              <div className="cart-content-container">
+                <h1 className="cart-heading">My Cart</h1>
+                <button
+                  type="button"
+                  className="remove-all-btn"
+                  onClick={onClickRemoveAllBtn}
+                >
+                  Remove All
+                </button>
+                <CartListView />
+                <CartSummary />
+              </div>
+            </div>
+          )}
+        </>
+      )
+    }}
+  </CartContext.Consumer>
+)
+export default Cart
